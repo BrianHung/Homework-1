abec2571ad9310beda1055d3d8214cc9974dd03a
Brian Hung <brianhung@berkeley.edu>
2017-09-15 10:21:23 -0700

    Implemented basic functions

--- ex1/greeter_skeleton.sol
+++ ex1/greeter_skeleton.sol
@@ -3,13 +3,20 @@ pragma solidity ^0.4.13;
 
 contract greeter {
 	/* Add one variable to hold our greeting */
-	string greeting;
+	string[] greetings;
+	uint public count = 0;
 
+    /* Write one line of code for the contract to set our greeting */
 	function greeter(string _greeting) public {
-		/* Write one line of code for the contract to set our greeting */
+		greetings.push(_greeting);
 	}
 
+    /* Write one line of code to allow the contract to return our greeting */
 	function greet() constant returns (string)  {
-		/* Write one line of code to allow the contract to return our greeting */
+		count += 1;
+		return greetings[count % greetings.length];
+	}
+
+	function() {
 	}
 }
--- ex2/fibonacci_skeleton.sol
+++ ex2/fibonacci_skeleton.sol
@@ -2,9 +2,21 @@ pragma solidity ^0.4.16;
 
 
 contract Fibonacci {
+
+    event FibCalcd(uint fib);
+
 	function calculate(uint position) returns (uint result) {
-		/* Add one variable to hold our greeting */
+        if (position < 2) {
+    	    result = position;
+    	} else {
+    	    uint tmp = calculate(position - 1) + calculate(position - 2);
+    	    result =  tmp;
+    	}
+    	FibCalcd(result);
+    	return result;
 	}
 
 	/* Add a fallback function to prevent contract payability and non-existent function calls */
+	function() {
+	}
 }
